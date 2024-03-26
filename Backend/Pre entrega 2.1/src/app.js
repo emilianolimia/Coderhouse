eb8d0cb5eb5bcaa467fbfd974d56d95ed33e5255
Emi <emilianolimia@gmail.com>
2024-03-25 23:56:27 -0300

    implement login

---
+++ Backend/Pre entrega 2.1/src/app.js
@@ -0,0 +1,160 @@
+const express = require('express');
+const exphbs = require('express-handlebars');
+const http = require('http');
+const socketio = require('socket.io');
+const path = require('path');
+const mongoose = require('mongoose');
+const session = require('express-session');
+
+// Importar models
+const messageModel = require('./models/message')
+const productModel = require('./models/product');
+const cartModel = require('./models/cart');
+
+// Importar los routers de productos y carritos
+const productRouter = require('./routes/productRouter');
+const cartRouter = require('./routes/cartRouter');
+const sessionsRouter = require('./routes/sessionRouter');
+
+const app = express();
+const port = 8080;
+
+// Middleware para parsear el cuerpo de la solicitud en formato JSON
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// Configuración de Handlebars
+app.engine('handlebars', exphbs.engine());
+app.set('view engine', 'handlebars');
+app.set('views', path.join(__dirname, 'views'));
+
+// Configuración de archivos estáticos
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Conexión con MongoDB
+mongoose.connect('mongodb+srv://emilimiadev:qKcR4pvMYlS89gTD@cluster0.mlbri5k.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0')
+    .then(() => console.log("DB is connected"))
+    .catch(e => console.log(e))
+
+app.use(session({
+  secret: 'tu_secreto',
+  resave: false,
+  saveUninitialized: true,
+}));
+
+// Usar los routers de productos y carritos en la aplicación
+app.use('/api/products', productRouter);
+app.use('/api/carts', cartRouter);
+app.use('/api/sessions', sessionsRouter);
+
+// Middleware para redirigir al endpoint /login
+app.use((req, res, next) => {
+  if (!req.session.user && req.originalUrl !== '/login') {
+    return res.redirect('/login');
+  }
+  next();
+});
+
+// Inicialización del servidor
+const server = http.createServer(app);
+const io = socketio(server);
+
+server.listen(port, () => {
+  console.log(`Servidor corriendo en ${port}`);
+});
+
+// Manejo de conexión de sockets
+io.on('connection', (socket) => {
+  console.log('Cliente conectado');
+
+  socket.on('message', async data => {
+    const newMessage = await messageModel.create(data);
+    res.status(201).send(newMessage);
+
+    const messages = await messageModel.find();
+    io.emit('messageLogs', messages)
+  })
+
+  // Manejo de eventos de creación de producto
+  socket.on('productCreated', () => {
+    io.emit('productListUpdated');
+  });
+
+  // Manejo de eventos de eliminación de producto
+  socket.on('productDeleted', () => {
+    io.emit('productListUpdated');
+  });
+});
+
+app.get('/products', async (req, res) => {
+  try {
+      const products = await productModel.find();
+      console.log(products);
+      res.render('home', { 
+        products: JSON.parse(JSON.stringify(products)),
+        user: req.session.user // Pasa el usuario al contexto de la vista
+      });
+    } catch (error) {
+      console.error('Error:', error.message);
+      res.status(500).json({ error: 'Error interno del servidor' });
+    }
+});
+
+app.get('/products/:pid', async (req, res) => {
+  try {
+      const productId = req.params.pid;
+      const product = await productModel.findById(productId).lean();
+
+  
+      if (product) {
+        res.render('productDetails', { product });
+      } else {
+        res.status(404).json({ error: 'Producto no encontrado' });
+      }
+    } catch (error) {
+      console.error('Error:', error.message);
+      res.status(500).json({ error: 'Error interno del servidor' });
+    }
+});
+
+app.get('/carts/:cid', async (req, res) => {
+  try {
+      const cartId = req.params.cid;
+  
+      // Obtener el carrito por ID con los productos completos mediante "populate"
+      const cart = await cartModel.findById(cartId).populate('products.id_prod');
+      if (!cart) {
+        return res.status(404).json({ error: 'Carrito no encontrado' });
+      }
+      console.log(cart);
+      res.render('cart', { cart: JSON.parse(JSON.stringify(cart)) });
+  } catch (error) {
+      console.error('Error:', error.message);
+      res.status(500).json({ error: 'Error interno del servidor' });
+  }
+});
+
+app.get('/profile', (req, res) => {
+  if (req.session.user) {
+    // Si el usuario está autenticado, muestra el perfil
+    res.render('profile', { user: req.session.user });
+  } else {
+    // Si el usuario no está autenticado, redirige al login
+    res.redirect('/login');
+  }
+});
+
+app.get('/login', (req, res) => {
+  res.render('login'); // Renderiza la vista de login
+});
+
+// Logout
+app.get('/logout', (req, res) => {
+  req.session.destroy((err) => {
+    if (err) {
+      console.error('Error al cerrar sesión:', err);
+      return res.status(500).json({ error: 'Error interno del servidor' });
+    }
+    res.redirect('/login');  // Redirige al usuario a la vista de login después de cerrar sesión
+  });
+});
---
+++ Backend/Pre entrega 2.1/src/routes/sessionRouter.js
@@ -0,0 +1,79 @@
+const express = require('express');
+const bcrypt = require('bcrypt');
+const User = require('../models/userModel');
+const router = express.Router();
+
+// Nueva ruta GET para el login
+router.get('/login', (req, res) => {
+  res.render('login'); // Renderiza la vista de login
+});
+
+// Registrar usuario
+router.post('/register', async (req, res) => {
+  try {
+    const hashedPassword = await bcrypt.hash(req.body.password, 10);
+    const newUser = new User({
+      first_name: req.body.first_name,
+      last_name: req.body.last_name,
+      email: req.body.email,
+      age: req.body.age,
+      password: hashedPassword,
+    });
+    await newUser.save();
+    console.log('Usuario registrado con éxito:', newUser);
+    res.status(201).json({ message: 'Usuario registrado con éxito' });
+  } catch (error) {
+    console.error('Error al registrar usuario:', error);
+    res.status(500).json({ error: 'Error interno del servidor' });
+  }
+});
+
+// Login
+router.post('/login', async (req, res) => {
+  try {
+    console.log('Intento de inicio de sesión para el email:', req.body.email);
+    
+    // Validación interna para el usuario admin
+    if (req.body.email === 'adminCoder@coder.com' && req.body.password === 'adminCod3r123') {
+      req.session.user = {
+        _id: 'admin',  // Id ficticio para el admin
+        first_name: 'Admin',
+        last_name: 'Coder',
+        email: 'adminCoder@coder.com',
+        age: 0,
+        role: 'admin',
+      };
+      console.log('Inicio de sesión exitoso como admin');
+      return res.redirect('/products');
+    }
+
+    // Si no es el usuario admin, entonces verifica en la base de datos
+    const user = await User.findOne({ email: req.body.email });
+    if (user && await bcrypt.compare(req.body.password, user.password)) {
+      req.session.user = {
+        _id: user._id,
+        first_name: user.first_name,
+        last_name: user.last_name,
+        email: user.email,
+        age: user.age,
+        role: user.email === 'adminCoder@coder.com' ? 'admin' : 'usuario',
+      };
+      console.log('Sesión establecida:', req.session.user);
+      return res.redirect('/products');
+    } 
+
+    res.status(401).json({ error: 'Credenciales incorrectas' });
+
+  } catch (error) {
+    console.error('Error interno del servidor:', error);
+    res.status(500).json({ error: 'Error interno del servidor' });
+  }
+});
+
+// Logout
+router.post('/logout', (req, res) => {
+  req.session.destroy();
+  res.status(200).json({ message: 'Cierre de sesión exitoso' });
+});
+
+module.exports = router;
