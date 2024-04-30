7cc4b3dffd52847315c928a9c8f717317119e74f
Emi <emilianolimia@gmail.com>
2024-04-29 23:55:03 -0300

    reestructura servidor

---
+++ Backend/Pre entrega 2.3/config.js
@@ -0,0 +1,7 @@
+const config = {
+    githubClientId: process.env.GITHUB_CLIENT_ID,
+    githubClientSecret: process.env.GITHUB_CLIENT_SECRET,
+    dbUrl: process.env.DB_URL
+  };
+  
+module.exports = config;
